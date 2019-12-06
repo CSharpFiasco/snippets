c86e144b3f05f8e520fd3311fa1182b1582bb8d2
carlos-martos <carloskmartos@gmail.com>
2019-12-05 19:46:23 -0600

    Starting python functions

---
+++ python/functions.py
@@ -0,0 +1,39 @@
+#POSTGRES DATABASE
+#DEPENDS ON psycopg2
+def make_conn():
+    conn = None
+    try:
+        conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (db_name, db_user, db_host, db_pass))
+        print("connected to db")
+    except:
+        print ("I am unable to connect to the database")
+    return conn
+
+def fetch_data(conn, query):
+    result = []
+    #print ("Now executing: %s" % (query))
+    cursor = conn.cursor()
+    cursor.execute(query)
+    
+    raw = cursor.fetchall()
+    for line in raw:
+        result.append(line)
+    
+    return result
+
+def set_data(conn, query):
+    #print ("Now executing: %s" % (query))
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+
+def toSQL(string):
+	# print(string)
+	string = str(string)
+	string.replace('\'', '\'\'')
+	return string
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
