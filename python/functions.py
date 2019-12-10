2dee85dbe70c4629fd41d2135b9ca982adfd0a98
carlos-martos <carloskmartos@gmail.com>
2019-12-09 22:45:36 -0600

    timeDeltaToSeconds python from import script

--- python/functions.py
+++ python/functions.py
@@ -39,3 +39,8 @@ def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.strptime(d2, "%Y-%m-%d")
     return abs((d2 - d1).days)
+
+#t: timeDelta from datetime library
+#returns decimal
+def timeDeltaToSeconds(t):
+    return t.total_seconds()
