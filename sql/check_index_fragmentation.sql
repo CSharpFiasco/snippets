151c87aee9b0cc8070092f0080875e28667e33fb
CSharpFiasco <40156600+CSharpFiasco@users.noreply.github.com>
2021-06-28 20:09:41 -0500

    Exclude indexes with low page counts

--- sql/check_index_fragmentation.sql
+++ sql/check_index_fragmentation.sql
@@ -10,4 +10,5 @@ INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
 INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
 AND indexstats.index_id = dbindexes.index_id
 WHERE indexstats.database_id = DB_ID()
+AND indexstats.page_count > 15
 ORDER BY indexstats.avg_fragmentation_in_percent desc
